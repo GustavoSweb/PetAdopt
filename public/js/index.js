2fadd3a87d8d9ec83beae3c43eb051d16d937fed
Gustavo <gustavodasilvama10@gmail.com>
2024-09-18 21:36:04 -0300

    Section service finishild

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,16 @@
 var menuLink = document.querySelectorAll(".menuLink");
-console.log(menuLink);
-menuLink.forEach((value) => {
-  value.addEventListener("click", () => {});
+var SectionMain = document.querySelector("#SectionMain");
+var PawPet = document.querySelector("#PawPet");
+var SectionServices = document.querySelector("#SectionServices");
+var value_main = 0;
+var value_main_left = 0;
+var value_pawpet = ["15px", "85px"];
+document.addEventListener("scroll", () => {
+  console.log("Teste-" + SectionMain.getBoundingClientRect().top);
+  if (SectionServices.getBoundingClientRect().top < 60) {
+    return (PawPet.style.left = value_pawpet[1]);
+  }
+  if (SectionMain.getBoundingClientRect().top < 60) {
+    return (PawPet.style.left = value_pawpet[0]);
+  }
 });
